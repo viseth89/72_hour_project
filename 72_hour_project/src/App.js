b7ec0405e50748d7416e9e93cb1970fb6e2245f3
viseth89 <viseth89@gmail.com>
2021-07-03 13:18:06 -0500

    First attempt of using location. Grabbed notes from https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API Currently receiving a Cannot read property addEvent listener of null

--- 72_hour_project/src/App.js
+++ 72_hour_project/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,9 +17,54 @@ function App() {
         >
           Learn React
         </a>
+
+        <button id="find-me">Show my location</button>
+        <br />
+        <p id="status"></p>
+        <a id="map-link" target="_blank"></a>
       </header>
+
+
+
+
+
+
+      
     </div>
   );
 }
 
 export default App;
+
+
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Unable to retrieve your location';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolocation is not supported by your browser';
+  } else {
+    status.textContent = 'Locating…';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', console.log('hi'));
