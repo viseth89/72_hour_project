f5f700d130c17497872ce40a874076660b7095ba
Ashley Shaw <a.ramboshaw@gmail.com>
2021-07-03 17:15:36 -0400

    made progress on vanillaJS fetch, which is working

--- 72_hour_project/src/Apps/NasaApp/NasaApp.js
+++ 72_hour_project/src/Apps/NasaApp/NasaApp.js
@@ -1,24 +1,23 @@
+//You guys, this works in vanilla JS.
+
+
 //let url = 'https://api.nasa.gov/planetary/earth/imagery?api_key=3IQa6mxWV7BJU9sdptABxOso27v5dcJI37jN8eyA'
 
 let testurl = 'https://api.nasa.gov/planetary/earth/imagery?lon=-86.4&lat=39.8&date=2021-02-01&api_key=3IQa6mxWV7BJU9sdptABxOso27v5dcJI37jN8eyA'
 
 
 //let key = '3IQa6mxWV7BJU9sdptABxOso27v5dcJI37jN8eyA'
-
+let satImage = document.querySelector('.satImage');
 
 fetch(testurl)
-  .then(response => response.blob())
-  .then(myBlob => {
-    testurl.src = URL.createObjectURL(myBlob);
-    console.log(myBlob);
-  })
+.then(function(response) {
+  console.log("Response:", response);
+  return response.blob();
+})
 
-  .then(function(myBlob) {
-    let satImage = document.createElement('img')
-    
-    satImage.innerHTML = myBlob;
-  
-
-
-    myBlob.append(satImage);
+.then(function(photoBlob) {
+  let objectURL = URL.createObjectURL(photoBlob);
+  console.log("Object URL:", objectURL);
+  satImage.src = objectURL;
+  console.log("satImage.src:", satImage.src);
 });
