a7ed0de4759a52a34d0d3bcd62ffc0d1c1943364
viseth89 <viseth89@gmail.com>
2021-07-05 18:19:53 -0500

    Working on saving coordinates, using bind and states

--- 72_hour_project/src/App.js
+++ 72_hour_project/src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude:null,
+      longitude:null,
+      userAddress:null
+    };
+    this.getLocation = this.getLocation.bind(this)
+    this.getCoordinates = this.getCoordinates.bind(this)
+  }
+
+  getLocation() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getCoordinates)
+    } else {
+      alert("Geolocation is not supported by this browser.")
+    }
+  }
+
+  getCoordinates(position) {
+    console.log(position)
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h2>
+          React Geolocation Exmaple
+        </h2>
+
+        <button onClick={this.getLocation}>Get coordinates</button>
+        <h4>HTML5 Coordinates</h4>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+        <h4>Google Maps Reverse Geocoding</h4>
+        <p>Address: {this.state.userAddress}</p>
+      </div>
+    )    
+  }
 }
+
+
+
+
+
 
 export default App;
