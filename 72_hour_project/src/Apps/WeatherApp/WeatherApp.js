38bd29f3e0e5d78b9adaa65287cb000d0bc88ee8
Sarah Tanner <sktanner92@gmail.com>
2021-07-05 14:58:35 -0400

    Displaying weather data

--- 72_hour_project/src/Apps/WeatherApp/WeatherApp.js
+++ 72_hour_project/src/Apps/WeatherApp/WeatherApp.js
@@ -2,38 +2,58 @@ import React, { useState, useEffect } from 'react'
 import './WeatherApp.css'
 
 const WeatherApp = () => {
-    const [results, setResults] = useState(null)
-    const [error, setError] = useState(null)
+    const [results, setResults] = useState([])
+    const [error, setError] = useState()
 
     useEffect(() => {
     let lat = 39.78
     let lon = -86.30
-    
-    const URL = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=eae82edb29f893b2de779e351325ef24'
+
+    const URL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=eae82edb29f893b2de779e351325ef24'
+
+        console.log(URL);
 
         fetch(URL)
-        // .then(res => res.json())
-        // .then(json => setData(json))
         .then((response) => {
             if (response.ok) {
                 return response.json()
             }
-            throw response
+        })
+        .then((data) => {
+            console.log(data);
+            setResults(data)
         })
         .catch((error) => {
             console.error("Error fetching data: ", error)
             setError(error)
         })
     }, [])
-
-    if (error) return "Error!"
     
-    return(
+    return (
         <div className="main">
-            <pre>{JSON.stringify(results)}</pre>
+            <div>{JSON.stringify(results.weather[0].description)}</div>
         </div>
     )
-    
+
 }
 
 export default WeatherApp
+
+
+
+//     let lat = 39.78
+//     let lon = -86.30
+
+//     useEffect(() => {
+//         fetchResults()
+//     }, [])
+
+//     const fetchResults = () => {
+//         const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=eae82edb29f893b2de779e351325ef24`
+
+//         fetch(URL)
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 console.log(data)
+//             })
+//     }
