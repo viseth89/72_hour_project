2394e8104ad6827278d8ec1bc7a1809acd34cc03
Sarah Tanner <sktanner92@gmail.com>
2021-07-04 23:38:56 -0400

    Tried useEffect and useState

--- 72_hour_project/src/Apps/WeatherApp/WeatherApp.js
+++ 72_hour_project/src/Apps/WeatherApp/WeatherApp.js
@@ -1,24 +1,36 @@
- import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './WeatherApp.css'
 
 const WeatherApp = () => {
-    // const [results, setResults] = useState([])
+    const [results, setResults] = useState(null)
+    const [error, setError] = useState(null)
 
-    const fetcher = () => {
+    useEffect(() => {
     let lat = 39.78
     let lon = -86.30
     
     const URL = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=eae82edb29f893b2de779e351325ef24'
 
         fetch(URL)
-        .then(res => res.json())
-        .then(json => console.log(json()))
-        .catch(err => console.log(err))
-    }
-    
+        // .then(res => res.json())
+        // .then(json => setData(json))
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw response
+        })
+        .catch((error) => {
+            console.error("Error fetching data: ", error)
+            setError(error)
+        })
+    }, [])
 
+    if (error) return "Error!"
+    
     return(
         <div className="main">
-            <button onClick={fetcher}>Click Me</button>
+            <pre>{JSON.stringify(results)}</pre>
         </div>
     )
     
